37b112d8c8316e71dd7babcb548f12eb7aa654a8
hidetonishi <next1993.4@gmail.com>
2019-10-09 15:14:07 +0900

    lesson10-4

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,64 @@
+$(function(){
+  function buildHTML(message) {
+    if ( message.image ) {
+      var html = `<div class="message">
+      <div class="message__upper-info">
+      <p class="message__upper-info__talker">
+      ${message.user_name}
+      </p>
+      <p class="message__upper-info__date">
+      ${message.date}
+      </p>
+      </div>
+      <p class="message__text">
+      ${message.content}
+      <img class="message__text__image" src= ${message.image} >
+      </p>
+      </div>`
+      return html;
+    } 
+    else {
+      var html = 
+      `<div class="message">
+      <div class="message__upper-info">
+      <p class="message__upper-info__talker">
+      ${message.user_name}
+      </p>
+      <p class="message__upper-info__date">
+      ${message.date}
+      </p>
+      </div>
+      <p class="message__text">
+      ${message.content}
+      </p>
+      </div>`
+      return html;
+    };
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+
+    .done(function(message){
+      var html = buildHTML(message);
+      $('.messages').append(html);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      $('.new_message')[0].reset()
+      $("submit-btn").prop("disabled", false)
+    })
+    .fail(function(){
+      alert('メッセージの送信に失敗しました');
+    });
+    return false;
+  });
+});
